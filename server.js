23d888ea090d2f9f40fbb6b0ba4d8ba755cda519
eeddee <edward@edwardsharp.net>
2019-12-29 06:35:51 -0500

    tidy'n

--- server.js
+++ server.js
@@ -28,10 +28,16 @@ passport.use(strategy)
 const app = express()
 
 const corsOptions = {
-  origin: '*',
+  origin: [
+    'http://localhost:3001',
+    'http://localhost:3002',
+    'http://localhost:3000',
+    'https://marshlife-art.github.io'
+  ],
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204, i guess.
 }
 app.use(cors(corsOptions))
+app.options('*', cors())
 
 app.use(passport.initialize())
 
