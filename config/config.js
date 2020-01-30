0a0f0aa00000758ec388045c9519f7df4139d9e6
eeddee <edward@edwardsharp.net>
2020-01-30 00:08:45 -0500

    better pg config

--- config/config.js
+++ config/config.js
@@ -6,6 +6,19 @@ module.exports = {
     password: process.env.PG_DATABASE_PASSWORD,
     database: process.env.PG_DATABASE_NAME,
     host: process.env.PG_DATABASE_HOST,
+    port: process.env.PG_DATABASE_PORT,
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true
+      }
+    },
+    pool: {
+      max: 20,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
     dialect: 'postgres'
   },
   test: {
@@ -21,6 +34,11 @@ module.exports = {
     dialect: 'postgres',
     logging: false,
     ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true
+      }
+    },
     pool: {
       max: 20,
       min: 0,
